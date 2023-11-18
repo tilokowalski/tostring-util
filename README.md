1da277c8dc04169c762bc0908b53700856375267
Tilo Kowalski <privat@tilokowalski.de>
2023-11-18 18:08:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,115 @@
+# ToString Utility for Java
+
+![Maven CI/CD Pipeline](https://github.com/tilokowalski/tostring-util/actions/workflows/maven-deploy.yml/badge.svg)
+
+## Introduction
+
+The `ToString` utility class in Java provides a customizable alternative to the traditional `toString()` method. It offers a human-readable string representation of any object, with options for single-line or multi-line formatting, resolving nested objects, and exploring class hierarchies.
+
+## Features
+
+- **Custom String Representation**: Generate a detailed string representation of any Java object.
+- **Flexible Formatting**: Choose between single-line or multi-line output.
+- **Class Hierarchy Exploration**: Option to explore any level of class hierarchy of the object.
+- **Handling of Nested Objects**: Ability to resolve nested objects (automatically preventing circular references).
+- **Custom Annotations**: Utilize `ToStringDontResolve` and `ToStringIgnore` annotations for finer control over the string representation.
+
+## Usage
+
+### Include Maven Dependency
+
+You can get the ToString Utility from Maven Central Repository. Add the following dependency to your `pom.xml` file:
+
+```xml
+<dependency>
+  <groupId>de.tilokowalski.util</groupId>
+  <artifactId>tostring-util</artifactId>
+</dependency>
+```
+
+### Create Basic String Representation
+
+To create a simple string representation of an object:
+
+```java
+YourClass yourObject = new YourClass();
+
+String simpleToString = ToString.create(yourObject);
+System.out.println(simpleToString);
+```
+
+### Create Detailed Dump
+
+For a detailed, multi-line representation, including nested objects:
+
+```java
+YourClass yourObject = new YourClass();
+
+String detailedDump = ToString.createDump(yourObject);
+System.out.println(detailedDump);
+```
+
+### Create Custom Representation
+
+Create a custom string representation with specific settings:
+
+```java
+ToString.createCustom(anyObject, delimiter, nesting, level, resolve);
+```
+
+```java
+YourClass yourObject = new YourClass();
+
+String customToString = ToString.createCustom(yourObject, ',', 0, ToString.TS_LEVEL_DEEP, true);
+System.out.println(customToString);
+```
+
+| Parameter | Description |
+|---|---|
+| `delimiter` | Character used to separate fields in the output. For example, `,` for single-line or `\n` for multi-line representation. |
+| `nesting` | Integer specifying the depth of the currently nested object. This will usually be `0`, except for custom indentation from the beginning.  |
+| `level` | Determines the level of class hierarchy to be explored. Use `ToString.TS_LEVEL_ONLY` for the object's direct fields, `ToString.TS_LEVEL_DEEP` for full hierarchy exploration, or any other integer. |
+| `resolve` | Boolean flag indicating whether to resolve nested objects. `true` will resolve them, while `false` will avoid resolving nested objects. |
+
+**Note:** Adjust these parameters according to the specific needs of your object's string representation.
+
+## Custom Annotations
+
+### `@ToStringDontResolve`
+
+Use this annotation on a field to prevent resolving it during the string conversion.
+
+```java
+public class YourClass {
+
+  @ToStringDontResolve
+  private List unresolvedList;
+
+  @ToStringDontResolve
+  private OtherClass unresolvedObject;
+
+  private String resolveMe;
+
+  // ...
+}
+```
+
+### `@ToStringIgnore`
+
+Apply this annotation to a field you wish to exclude from the string representation.
+
+```java
+public class YourClass {
+
+  @ToStringIgnore
+  private String ignoredField;
+
+  private String includedField;
+
+  // ...
+}
+```
+
+---
+
+**Note:** The `ToString` utility class requires Java Reflection for its operations.
---
+++ src/main/java/de/tilokowalski/util/ToString.java
@@ -0,0 +1,409 @@
+package de.tilokowalski.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InaccessibleObjectException;
+import java.lang.reflect.Modifier;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * This class provides a utility to generate a human-readable string representation of any object.
+ * It's a customizable alternative to the traditional toString() method.
+ */
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ToString {
+
+    /**
+     * Constant representing the opening parenthesis of the objects content.
+     */
+    private static final char TS_PARANTHESIS_OPEN = '[';
+
+    /**
+     * Constant representing the closing parenthesis of the objects content.
+     */
+    private static final char TS_PARANTHESIS_CLOSE = ']';
+
+    /**
+     * Constant representing the quotation mark, used to encase string values.
+     */
+    private static final char TS_PARANTHESIS_STRING = '\"';
+
+    /**
+     * Constant representing the equal sign between field name and value.
+     */
+    private static final char TS_EQUALS = '=';
+
+    /**
+     * Constant representing the single line delimiter which separates fields when creating a single line string representation.
+     */
+    public static final char TS_DELIMITER_SL = ',';
+
+    /**
+     * Constant representing the multi-line delimiter which separates fields when creating a multi-line string representation.
+     */
+    public static final char TS_DELIMITER_ML = '\n';
+
+    /**
+     * Constant representing only the object itself as the level of class hierarchy to be explored when creating a string representation.
+     */
+    public static final int TS_LEVEL_ONLY = 0;
+
+    /**
+     * Constant representing the entire class hierarchy to be explored when creating a string representation.
+     */
+    public static final int TS_LEVEL_DEEP = -1;
+
+    /**
+     * Constant representing a null value for a field.
+     */
+    public static final String TS_EXPR_NULL = "NULL";
+
+    /**
+     * Constant representing a circular reference to a parent object, used to avoid infinite recursion.
+     */
+    public static final String TS_EXPR_CIRC_REF = "PARENT";
+
+    /**
+     * The object for which the string representation will be generated.
+     */
+    @Getter
+    @Setter
+    private Object object;
+
+    /**
+     * The character to be used as delimiter between fields.
+     */
+    @Getter
+    @Setter
+    private char delimiter;
+
+    /**
+     * Depth of the nested object, used for indentation.
+     */
+    @Getter
+    @Setter
+    private int nesting;
+
+    /**
+     * Level of class hierarchy to be explored.
+     */
+    @Getter
+    @Setter
+    private int level;
+
+    /**
+     * Whether to resolve nested objects or not.
+     */
+    @Getter
+    @Setter
+    private boolean resolve;
+
+    /**
+     * List of objects that have already been resolved during the string conversion.
+     */
+    @Getter
+    private static ArrayList<Object> alreadyResolved = new ArrayList<Object>();
+
+    /**
+     * Overrides the toString method to return a string representation of the object.
+     *
+     * @return String representation of the object.
+     */
+    @Override
+    public String toString() {
+        try {
+            return this.buildFromAttributes();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Private method to build string representation from object's attributes.
+     * Uses indirect recursion to implement nested objects.
+     *
+     * @return String representation of the object's attributes.
+     * @throws Exception When any error occurs while generating the string.
+     */
+    private String buildFromAttributes() throws Exception {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(getObject().getClass().getSimpleName());
+        stringBuilder.append(TS_PARANTHESIS_OPEN);
+
+        if (getNesting() == 0) {
+            getAlreadyResolved().clear();
+        }
+
+        if (!getAlreadyResolved().contains(object)) {
+            if (getObject() instanceof Collection<?>) {
+                Collection<?> collection = (Collection<?>) getObject();
+                stringBuilder = appendCollection(stringBuilder, collection);
+            } else if (getObject() instanceof Map<?, ?>) {
+                Collection<?> collection = ((Map<?, ?>) getObject()).values();
+                stringBuilder = appendCollection(stringBuilder, collection);
+            } else if (getObject().getClass().isArray()) {
+                Collection<?> collection = Arrays.asList(getObject());
+                stringBuilder = appendCollection(stringBuilder, collection);
+            } else {
+                if (getDelimiter() == ToString.TS_DELIMITER_ML) {
+                    stringBuilder.append(getDelimiter());
+                }
+
+                boolean firstField = true;
+                for (Field field : getDeclaredFields(getObject(), getLevel())) {
+                    validateFieldAnnotations(field);
+
+                    if (getNesting() > 0 && !isResolve()) {
+                        continue;
+                    }
+
+                    if (!isFieldIncluded(field)) {
+                        continue;
+                    }
+
+                    if (!firstField) {
+                        stringBuilder.append(getDelimiter());
+                    }
+
+                    if (getDelimiter() == ToString.TS_DELIMITER_ML) {
+                        stringBuilder.append(getIndentation(getNesting()));
+                    }
+
+                    stringBuilder = appendValue(stringBuilder, field, getObject());
+
+                    if (firstField) {
+                        firstField = false;
+                    }
+                }
+
+                if (getDelimiter() == ToString.TS_DELIMITER_ML) {
+                    stringBuilder.append(getDelimiter());
+                    stringBuilder.append(getIndentation(getNesting() - 1));
+                }
+            }
+        } else {
+            stringBuilder.append(TS_EXPR_CIRC_REF);
+        }
+
+        stringBuilder.append(TS_PARANTHESIS_CLOSE);
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Get the declared fields for a given object up to a certain level.
+     *
+     * @param object The object for which to get declared fields.
+     * @param level The maximum level up to which to get fields.
+     * @return A list of Fields for the object.
+     */
+    private ArrayList<Field> getDeclaredFields(Object object, int level) {
+        ArrayList<Field> result = new ArrayList<Field>();
+
+        Class<?> currentClass = object.getClass();
+
+        int iteration = 0;
+
+        while (currentClass != null) {
+            for (Field field : currentClass.getDeclaredFields()) {
+
+                try {
+                    field.setAccessible(true);
+
+                    if (currentClass.isArray() || Collection.class.isAssignableFrom(currentClass) || Map.class.isAssignableFrom(currentClass)) {
+                        if (!field.getName().equals("elementData")) {
+                            continue;
+                        }
+                    }
+
+                    result.add(field);
+                } catch (InaccessibleObjectException e) {
+                    /* IGNORE */
+                }
+
+            }
+
+            currentClass = currentClass.getSuperclass();
+
+            if (level != TS_LEVEL_DEEP && iteration == level) {
+                break;
+            }
+            iteration++;
+        }
+
+        return result;
+    }
+
+    /**
+     * Check if a given field should be included in the string representation.
+     * Depends on ignore annotation and final and static modifiers but not on visibility.
+     *
+     * @param field The field to check.
+     * @return Whether the field should be included in the string representation.
+     */
+    private boolean isFieldIncluded(Field field) {
+        if (Modifier.isFinal(field.getModifiers())) {
+            return false;
+        }
+
+        if (Modifier.isStatic(field.getModifiers())) {
+            return false;
+        }
+
+        if (field.getAnnotation(ToStringIgnore.class) != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Validate the annotations on a given field to detect incompabilities.
+     *
+     * @param field The field to validate.
+     * @throws Exception When the field has incompatible annotations.
+     */
+    private void validateFieldAnnotations(Field field) throws Exception {
+        if (field.getAnnotation(ToStringDontResolve.class) != null) {
+            if (field.getAnnotation(ToStringIgnore.class) != null) {
+                throw new Exception("annotations " + ToStringIgnore.class.getSimpleName() + " and " + ToStringDontResolve.class.getSimpleName() + " are not compatible");
+            }
+
+            if (!Collection.class.isAssignableFrom(field.getType()) && !Map.class.isAssignableFrom(field.getType()) && !field.getClass().isArray()) {
+                throw new Exception("annotation " + ToStringDontResolve.class + " is not supported for field " + field.getName() + " of type " + field.getType().getSimpleName());
+            }
+        }
+    }
+
+    /**
+     * Append the value of a given field to the StringBuilder that is passed through.
+     *
+     * @param result The StringBuilder to append to.
+     * @param field The field whose value to append.
+     * @param object The object from which to get the field's value.
+     * @return The updated StringBuilder.
+     */
+    private StringBuilder appendValue(StringBuilder result, Field field, Object object) {
+        result.append(field.getName());
+        result.append(TS_EQUALS);
+
+        Object value = null;
+
+        try {
+            value = field.get(object);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (value == null) {
+            result.append(TS_EXPR_NULL);
+        } else if (field.getType().isPrimitive()) {
+            result.append(value);
+        } else if (field.getType() == String.class) {
+            result.append("" + TS_PARANTHESIS_STRING + value + TS_PARANTHESIS_STRING);
+        } else if (field.getType() == Date.class) {
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            result.append(format.format(value));
+        } else if (field.getType() == Integer.class) {
+            result.append((int) value);
+        } else {
+            boolean resolve = isResolve();
+
+            if (field.isAnnotationPresent(ToStringDontResolve.class)) {
+                resolve = false;
+            }
+
+            result.append(ToString.createCustom(value, getDelimiter(), getNesting() + 1, getLevel(), resolve));
+        }
+
+        ToString.alreadyResolved.add(object);
+
+        return result;
+    }
+
+    /**
+     * Append the elements of a collection to the StringBuilder that is passed through.
+     *
+     * @param result The StringBuilder to append to.
+     * @param collection The collection whose elements to append.
+     * @return The updated StringBuilder.
+     */
+    private StringBuilder appendCollection(StringBuilder result, Collection<?> collection) {
+        if (collection.isEmpty() || !isResolve()) {
+            result.append(collection.size());
+        } else {
+            for (Object entry : collection) {
+                result.append(getDelimiter());
+                result.append(getIndentation(getNesting()));
+                result.append(ToString.createCustom(entry, getDelimiter(), getNesting() + 1, getLevel(), isResolve()));
+            }
+
+            result.append(getDelimiter());
+            result.append(getIndentation(getNesting() - 1));
+        }
+
+        return result;
+    }
+
+    /**
+     * Generate a string of a certain number of indentation characters.
+     *
+     * @param n The number of indentation characters to generate.
+     * @return A string of n indentation characters.
+     */
+    private static String getIndentation(int n) {
+        return new String("\t").repeat(n + 1);
+    }
+
+    /**
+     * Adds an object to the list of objects that have already been resolved in the string conversion.
+     *
+     * @param alreadyResolved An object that has been already resolved.
+     */
+    public static void addAlreadyResolved(Object alreadyResolved) {
+        ToString.alreadyResolved.add(alreadyResolved);
+    }
+
+    /**
+     * Generates a string representation of an object using single line delimiter and does not resolve nested objects.
+     *
+     * @param object The object to be represented as a string.
+     * @return String representation of the object.
+     */
+    public static String create(Object object) {
+        return (new ToString(object, TS_DELIMITER_SL, 0, TS_LEVEL_DEEP, false)).toString();
+    }
+
+    /**
+     * Generates a string representation of an object using multi-line identations and resolves nested objects.
+     *
+     * @param object The object to be represented as a string.
+     * @return String representation of the object.
+     */
+    public static String createDump(Object object) {
+        return (new ToString(object, TS_DELIMITER_ML, 0, TS_LEVEL_DEEP, true).toString());
+    }
+
+    /**
+     * Generates a custom string representation of an object using specified custom parameters.
+     *
+     * @param object The object to be represented as a string.
+     * @param delimiter Tthe character to be used as delimiter.
+     * @param nesting The depth of the nested object, used for indentation.
+     * @param level The level of class hierarchy to be explored.
+     * @param resolve Whether to resolve nested objects or not.
+     * @return String representation of the object.
+     */
+    public static String createCustom(Object object, char delimiter, int nesting, int level, boolean resolve) {
+        return (new ToString(object, delimiter, nesting, level, resolve)).toString();
+    }
+
+}
